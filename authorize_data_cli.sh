3809a277dbe3876b507b5955acf705cd587ab44f
Dmytro Gierman <mryakzp@gmail.com>
2018-02-13 18:37:58 +0200

    fix travis conf..... added data-cli user credentials

---
+++ authorize_data_cli.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Exporting 'core' user credentials.";
+echo "Don't worry, it is secured!";
+cat <<EOF > ~/.config/datahub/config.json
+{
+  "token": ${DATAHUB_TOKEN},
+  "profile": {
+    "avatar_url": "https://avatars0.githubusercontent.com/u/11707682?v=4",
+    "email": "mryakzp@gmail.com",
+    "id": "a08d3588fbae0355042537595c65819d",
+    "join_date": "Thu, 07 Dec 2017 09:12:52 GMT",
+    "name": "Dima German",
+    "provider_id": "github:11707682",
+    "username": "AcckiyGerman"
+  }
+}
+EOF
